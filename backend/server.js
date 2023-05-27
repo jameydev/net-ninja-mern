590068ef1358153865c0e3134b18c6087a11f068
jameydev <jybryce4@gmail.com>
2023-05-27 07:37:40 -0500

    Workout model added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // creates express app
@@ -14,10 +15,19 @@ app.use((req, res, next) => {
     next();
 });
 
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            // listen for requests
+            app.listen(process.env.PORT, () => {
+                console.log('MongoDB Connected!'.green)
+                console.log(`Listening on port ${process.env.PORT}`.green);
+            });
+        })
+        .catch(err => {
+            console.error(err.red);
+        });
+
 // Route handlers
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-});
