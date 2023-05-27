8f463075dbff22abe58ca8c6c4f70834a7c310a6
jameydev <jybryce4@gmail.com>
2023-05-27 07:50:21 -0500

    started on workout controller

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,47 @@
+const Workout = require('../models/Workout');
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+    res.status(200).json(workouts);
+}
+
+// get a single workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params;
+
+    const workout = await Workout.findById(id);
+    if (!workout) {
+        return res.status(404).json({ error: `No workout with id: ${id}`});
+    }
+
+    res.status(200).json(workout);
+}
+
+// create a new workout
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body;
+
+    try {
+        const workout = await Workout.create({
+            title,
+            load,
+            reps
+        });
+        res.status(200).json(workout);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+// delete a workout
+
+
+// update a workout
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+};
